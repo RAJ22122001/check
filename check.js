e456e7e5e8487c5742f859f3b9e34434b00dd8ce
Raj Kumar4 <raj_kumar4@epam.com>
2025-01-15 11:54:32 +0530

    add new

---
+++ check.js
@@ -0,0 +1,8 @@
+// Define a function to greet a person
+function greet(name) {
+    console.log("Hello, " + name + "!again ");
+  }
+  
+  // Call the function with a name as an argument
+  greet("Raj");
+  
